74da124d826b41dda049d4a4e84f8cb0523d44a7
Your Name <you@example.com>
2024-07-03 17:23:03 +0800

    Refactor code and update file paths in fuzzware_harness

--- emulator/harness/fuzzware_harness/native/native_hooks.c
+++ emulator/harness/fuzzware_harness/native/native_hooks.c
@@ -309,6 +309,13 @@ bool get_fuzz(uc_engine *uc, uint8_t *buf, uint32_t size) {
   printf("[NATIVE FUZZ] Requiring %d fuzz bytes\n", size);
   fflush(stdout);
 #endif
+#ifdef MYDEBUG
+char mybuf[100];
+uint32_t myipsr = 0;
+uc_reg_read(uc, UC_ARM_REG_IPSR, &myipsr);
+sprintf(mybuf, "myipsr = %x\n", myipsr);
+my_debug_log(mybuf);
+#endif
 
   // Deal with copying over the (remaining) fuzzing bytes
   if (size && fuzz_cursor + size <= fuzz_size) {
@@ -1812,6 +1819,7 @@ bool fifo_get_fuzz(uc_engine *uc, DataTracker *dt, uint8_t *buf,
   // my_debug_log("fifo_get_fuzz\n");
   // dt->fifo_tail += memcpy_size;
   // return false;
+
   return get_fuzz(uc, buf, size);
 }
 
